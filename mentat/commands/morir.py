27dd136d0458763d7ed68bb6bde4c54f08ccb1bf
César Rodríguez <cesar.idaho@gmail.com>
2023-10-29 12:28:32 +0100

    Make lint happy

--- mentat/commands/morir.py
+++ mentat/commands/morir.py
@@ -2,13 +2,13 @@
 import sys
 # import logging
 # import argparse
-from mentat.config import Config
 from irc.client import ServerConnection
+from mentat.config import Config
 
 
 def morir(connection: ServerConnection, event, args, config: Config):
     """Disconnect from the server and dies."""
-    if config.is_admin(event.source.nick) == False:
+    if not config.is_admin(event.source.nick):
         return
     # We ignore the args. To be implemented in the future.
     connection.disconnect("Ouch!!")
