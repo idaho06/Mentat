e7fa6076d5dca1e2a30a50d6a30d3aa3107a6aec
César Rodríguez <cesar.idaho@gmail.com>
2026-09-21 11:54:28 +0200

    Fix crash on bad arguments to dados and hola

--- mentat/commands/dados.py
+++ mentat/commands/dados.py
@@ -2,12 +2,15 @@
 
 """ Module for the dados command. """
 
-import io
-import sys
 import logging
-import argparse
 import random
 from irc.client import ServerConnection
+from mentat.commands.common import (
+    BotArgumentParser,
+    parse_command_args,
+    reply_target,
+    send_lines,
+)
 
 
 def _throw_dice(dice, number):
@@ -30,17 +33,11 @@ def dados(connection: ServerConnection, event, args: list):
     logging.debug("Entering dados function")
     logging.debug("Event: %s, Args: %s", event, args)
 
-    nick = event.source.nick
-    talk_to = None
-    if event.type == "privmsg":
-        talk_to = nick
-    else:
-        talk_to = event.target
+    talk_to = reply_target(event)
 
-    parser = argparse.ArgumentParser(
+    parser = BotArgumentParser(
         description="Comando de tirar dados",
         prog="dados",
-        exit_on_error=False,
     )
     parser.add_argument(
         "-d",
@@ -54,28 +51,9 @@ def dados(connection: ServerConnection, event, args: list):
         "-n", "--number", type=int, help="Número de dados a tirar", default=1
     )
 
-    # Redirect stdout to capture the help text
-    old_stdout = sys.stdout
-    sys.stdout = io.StringIO()
-
-    help_text = ""
-    dados_args = argparse.Namespace()
-    try:
-        dados_args = parser.parse_args(args)
-    except SystemExit:
-        help_text = sys.stdout.getvalue()
-    except argparse.ArgumentError as exc:
-        # Get the help text
-        help_text = sys.stdout.getvalue()
-        # Add the error message to the help text
-        help_text += f"\n{exc}"
-    finally:
-        sys.stdout = old_stdout
-
-    if help_text != "":
-        logging.debug("Help text: %s", help_text)
-        for help_line in help_text.splitlines():
-            connection.privmsg(talk_to, help_line)
+    dados_args, help_lines = parse_command_args(parser, args)
+    if dados_args is None:
+        send_lines(connection, talk_to, help_lines)
         return
 
     logging.debug("Dice: %s, Number: %s", dados_args.dice, dados_args.number)
--- mentat/commands/hola.py
+++ mentat/commands/hola.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 """Module for the hola command."""
 
-import io
-import sys
 import logging
-import argparse
 from irc.client import ServerConnection
+from mentat.commands.common import (
+    BotArgumentParser,
+    parse_command_args,
+    reply_target,
+    send_lines,
+)
 
 
 def hola(connection: ServerConnection, event, args: list):
@@ -14,16 +17,11 @@ def hola(connection: ServerConnection, event, args: list):
         "Entering hola function: c: %s, e: %s, args: %s", connection, event, args
     )
     nick = event.source.nick
-    talk_to = None
-    if event.type == "privmsg":
-        talk_to = nick
-    else:
-        talk_to = event.target
+    talk_to = reply_target(event)
 
-    parser = argparse.ArgumentParser(
+    parser = BotArgumentParser(
         description="Comando de saludo",
         prog="hola",
-        exit_on_error=False,
     )
 
     parser.add_argument(
@@ -35,32 +33,12 @@ def hola(connection: ServerConnection, event, args: list):
         required=False,
     )
 
-    # Redirect stdout to capture the help text
-    old_stdout = sys.stdout
-    sys.stdout = io.StringIO()
+    hola_args, help_lines = parse_command_args(parser, args)
+    if hola_args is None:
+        send_lines(connection, talk_to, help_lines)
+        return
 
-    help_text = ""
-    hola_args = argparse.Namespace()
-    try:
-        hola_args = parser.parse_args(args)
-    except SystemExit:
-        # Get the help text
-        help_text = sys.stdout.getvalue()
-    except argparse.ArgumentError as exc:
-        # Get the help text
-        help_text = sys.stdout.getvalue()
-        # Add the error message to the help text
-        help_text += f"\n{exc}"
-    finally:
-        # Restore stdout
-        sys.stdout = old_stdout
-
-    if help_text != "":
-        logging.debug("Help text: %s", help_text)
-        for help_line in help_text.splitlines():
-            connection.privmsg(talk_to, help_line)
+    if hola_args.nick:
+        connection.privmsg(talk_to, "Hola, " + hola_args.nick)
     else:
-        if hola_args.nick:
-            connection.privmsg(talk_to, "Hola, " + hola_args.nick)
-        else:
-            connection.privmsg(talk_to, "Hola, " + nick)
+        connection.privmsg(talk_to, "Hola, " + nick)
